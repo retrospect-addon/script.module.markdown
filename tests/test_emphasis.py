3019a47c334ebb0d2de72f45e68bd085eb65374b
Bas Rieter <basrieter@gmail.com>
2020-12-06 23:10:39 +0100

    Updated: Multiple emphasis in one line.

--- tests/test_emphasis.py
+++ tests/test_emphasis.py
@@ -33,35 +33,28 @@ class Emphasis(unittest.TestCase):
         result = converter.to_kodi(md_txt)
         self.assertEqual(kodi_text, result)
 
-    def mixed_emphasis_b_i(self):
+    def test_italic_double(self):
+        md_txt = "Some *italic* text and some more *italic* text"
+        kodi_text = "Some [I]italic[/I] text and some more [I]italic[/I] text"
+
+        result = converter.to_kodi(md_txt)
+        self.assertEqual(kodi_text, result)
+
+    def test_mixed_emphasis_b_i(self):
         md_txt = "Some **_italic_** text"
         kodi_text = "Some [B][I]italic[/I][/B] text"
 
         result = converter.to_kodi(md_txt)
         self.assertEqual(kodi_text, result)
 
-    def mixed_emphasis_b_i_all_stars(self):
-        md_txt = "Some ***italic*** text"
-        kodi_text = "Some [B][I]italic[/I][/B] text"
-
-        result = converter.to_kodi(md_txt)
-        self.assertEqual(kodi_text, result)
-
-    def mixed_emphasis_b_i_all_underscores(self):
-        md_txt = "Some ___italic___ text"
-        kodi_text = "Some [B][I]italic[/I][/B] text"
-
-        result = converter.to_kodi(md_txt)
-        self.assertEqual(kodi_text, result)
-
-    def mixed_emphasis_i_underscore_b_stars(self):
+    def test_mixed_emphasis_i_underscore_b_stars(self):
         md_txt = "Some _**italic**_ text"
         kodi_text = "Some [I][B]italic[/B][/I] text"
 
         result = converter.to_kodi(md_txt)
         self.assertEqual(kodi_text, result)
 
-    def mixed_emphasis_i_star_b_underscores(self):
+    def test_mixed_emphasis_i_star_b_underscores(self):
         md_txt = "Some *__italic__* text"
         kodi_text = "Some [I][B]italic[/B][/I] text"
 
